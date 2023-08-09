9c379849313fd49ff865da101d1f3852bfd2b6ff
nafulapa <125344635+nafulapa@users.noreply.github.com>
2023-08-09 03:46:27 -0700

    Add files via upload

---
+++ search movies/js/script.js
@@ -0,0 +1,10 @@
+let tvShow = [
+"The Mr. lesley-Ann",
+"Mr. and Mrs. Smith",
+"The David Tennant",
+"The Broker John"
+]
+let show = tvShow.find(show =>{
+    return show.includes('Mrs')
+})
+let show2 = show.find(m.indexOF('The') === 0);
